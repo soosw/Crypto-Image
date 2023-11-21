9084d3a5825e51d0b9212f7d8400ff181c0d1a9a
soosw <104663457+soosw@users.noreply.github.com>
2023-11-21 14:44:32 -0600

    bug arreglado
    Se preparo para más casos de error

--- encriptar.py
+++ encriptar.py
@@ -1,39 +1,39 @@
-# try block to handle exception
+from PIL import Image
+import io
+
 try:
-    # take path of image as a input
+    # to handle the path with escape characters
     path = input(r'Enter path of Image : ')
-     
+
     # taking encryption key as input
     key = int(input('Enter Key for encryption of Image : '))
-     
+
     # print path of image file and encryption key that
     # we are using
     print('The path of file : ', path)
     print('Key for encryption : ', key)
-     
-    # open file for reading purpose
-    fin = open(path, 'rb')
-     
-    # storing image data in variable "image"
-    image = fin.read()
-    fin.close()
-     
-    # converting image into byte array to
-    # perform encryption easily on numeric data
-    image = bytearray(image)
- 
-    # performing XOR operation on each value of bytearray
-    for index, values in enumerate(image):
-        image[index] = values ^ key
- 
-    # opening file for writing purpose
-    fin = open(path, 'wb')
-     
-    # writing encrypted data in image
-    fin.write(image)
-    fin.close()
-    print('Encryption Done...')
- 
-     
-except Exception:
-    print('Error caught : ', Exception.__name__)
+
+    # open image using PIL
+    with Image.open(path) as img:
+        # convert image to byte array
+        img_byte_array = io.BytesIO()
+        img.save(img_byte_array, format=img.format)
+        image = bytearray(img_byte_array.getvalue())
+
+        # performing XOR operation on each value of bytearray
+        for index, values in enumerate(image):
+            image[index] = values ^ key
+
+        # open image for writing purpose
+        with open(path, 'wb') as fin:
+            # writing encrypted data in image
+            fin.write(image)
+
+        print('Encryption Done...')
+
+except FileNotFoundError:
+    print(f'Error: File not found at path {path}')
+except PermissionError:
+    print(f'Error: Permission denied for file at path {path}')
+except Exception as e:
+    print(f'Error caught: {e.__class__.__name__} - {e}')
